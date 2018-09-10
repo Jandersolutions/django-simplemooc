a6412b538e4a7ddf167df66ddc19011e546ba475
Janderson B Abreu <jba1719@gmail.com>
2018-09-09 22:11:05 -0300

    criado model courses

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from django.conf.global_settings import MEDIA_ROOT
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -124,3 +127,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')

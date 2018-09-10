b32223380d9dd04aed5f62d6c5ccf3a763c19664
Janderson B Abreu <jba1719@gmail.com>
2018-09-10 12:56:20 -0300

    configurado carregamento de imagens e elementos estaticos

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Course
 
-# Create your views here.
+def index(request):
+    # Course -> banco de dados | Objects -> manager do bd | all-> metodo do objects
+    courses = Course.objects.all()
+    template_name = 'courses/index.html'
+    # context -> recebe variaveis que serao passadas ao render
+    context = {
+        'courses':courses
+    }
+    return render(request,template_name, context)
--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -13,10 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from logging import DEBUG
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
+from simplemooc.settings import MEDIA_ROOT, MEDIA_URL
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(('simplemooc.core.urls','core'),namespace='core'))
+    path('',include(('simplemooc.core.urls','core'),namespace='core')),
+    path('cursos/',include(('simplemooc.courses.urls','courses'),namespace='courses'))
 ]
+
+# somente para ambiente de desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

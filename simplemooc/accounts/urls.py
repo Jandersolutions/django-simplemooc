1ff476be02f0c9771cd31972fc226d931413c561
Janderson B Abreu <jba1719@gmail.com>
2018-09-11 22:21:18 -0300

    alteração no design

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView,LogoutView
 from . import views
+from . views import register
 
 urlpatterns = [
     path('entrar/',auth_views.LoginView.as_view(template_name='accounts/login.html'),name='login'),
     path('sair/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('cadastrar/',register,name='register')
 ]
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.conf.global_settings import LOGIN_URL
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def register(request):
+    template_name = 'accounts/register.html'
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    context = {
+        'form' : form
+    }
+    return render(request,template_name,context)
+
+
